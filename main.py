f0fc10aa1de0ec4b1ff69a2a8fec7b65f1d16c0f
Daniel du Preez <dandupreez@legiondev.co.za>
2024-01-01 18:56:37 +0200

    Student solution

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ url = "https://api.nasa.gov/planetary/apod?" \
 # Get the request data as a dictionary
 response1 = requests.get(url)
 data = response1.json()
+
+# Extract the image title, url and explanation
+title = data["title"]
+image_url = data["url"]
+explanation = data["explanation"]
+
+# Download the image
+image_filepath = "img.png"
+response2 = requests.get(image_url)
+with open(image_filepath, 'wb') as file:
+    file.write(response2.content)
+
+st.title(title)
+st.image(image_filepath)
+st.write(explanation)
